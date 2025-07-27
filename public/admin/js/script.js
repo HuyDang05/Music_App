29f82bedd60f8e8bd71e15e66f22366dd37a173d
HuyDang05 <quanghuy23052005@gmail.com>
2025-07-27 16:08:07 +0700

    Add Upload Audio and listen-preview

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -13,3 +13,22 @@ if (uploadImage) {
 
 }
 //End Upload Image
+
+//Upload Audio
+const uploadAudio = document.querySelector("[upload-audio]");
+if (uploadAudio) {
+    const uploadAudioInput = document.querySelector("[upload-audio-input]");
+    const uploadAudioPlay = document.querySelector("[upload-audio-play]");
+    const source = uploadAudio.querySelector("source");
+
+    uploadAudioInput.addEventListener("change", (e) => {
+        if (e.target.files.length) {
+            const audio = URL.createObjectURL(e.target.files[0]);
+
+            source.src = audio;
+            uploadAudioPlay.load();
+        }
+    });
+
+}
+//End Upload Audio
